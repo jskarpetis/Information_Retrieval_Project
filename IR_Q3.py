d3d2f960dfc03baec995a0f5fa52afdb3dd30030
John Skarpetis <73351036+jskarpetis@users.noreply.github.com>
2022-07-31 21:44:32 +0300

    Add files via upload

--- IR_Q3.py
+++ IR_Q3.py
@@ -1,14 +1,19 @@
-import nltk
+from base64 import encode
 from elasticsearch import Elasticsearch
 import re
-import ast
-from nltk import word_tokenize
-from gensim.models import Word2Vec
-import tensorflow
+import numpy as np
+
 from tensorflow import keras
 from tensorflow.keras import layers
+from keras.preprocessing.text import one_hot
+from keras.preprocessing.sequence import pad_sequences
+# from tensorflow.keras.preprocessing import text_dataset_from_directory
 from sklearn.decomposition import PCA
 from matplotlib import pyplot
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.layers.embeddings import Embedding
 elasticsearch = Elasticsearch(host="localhost", port=9200)
 
 
@@ -17,7 +22,7 @@ def basic_search(field, value):
     try:
         res = elasticsearch.search(index="bx-books", from_=0, size=10000, query={
             "match": {
-              f"{field}": f"{value}"
+                f"{field}": f"{value}"
             }},
             sort=[
                 {
@@ -25,7 +30,7 @@ def basic_search(field, value):
                         "order": "desc"
                     }
                 }
-            ]
+        ]
         )
     except:
         print("There has been an error in retrieving the data!")
@@ -42,20 +47,20 @@ def user_rating_search(uid, isbn):
     try:
         res = elasticsearch.search(index="bx-book-ratings", query={
             "bool": {
-              "must": [
-                {"match": {
-                  "isbn": f"{isbn}"
-                  }
-                },
-                {
-                  "match": {
-                    "uid": f"{uid}"
-                  }
-                }
+                "must": [
+                    {"match": {
+                        "isbn": f"{isbn}"
+                    }
+                    },
+                    {
+                        "match": {
+                            "uid": f"{uid}"
+                        }
+                    }
 
-              ]
+                ]
             }
-          })
+        })
     except:
         print("There has been an error in retrieving the data!")
 
@@ -74,7 +79,8 @@ def tokenize_sentence(sentence):
     sentence_strip_stars = sentence_strip_marks.replace("*", "")
 
     sentence_strip_numbers = re.sub(r"[0-9]+", "", sentence_strip_stars)
-    final_sentence = re.sub(r"!|@|#|$|%|^|&|:|;|'|<|>|/|-|=", "", sentence_strip_numbers)
+    final_sentence = re.sub(
+        r"!|@|#|$|%|^|&|:|;|'|<|>|/|-|=", "", sentence_strip_numbers)
 
     split_sentence = re.split('\s+', final_sentence)
     # new_sentence = [word_tokenize(word) for word in split_sentence]
@@ -85,16 +91,16 @@ def find_rated_books(uid):
 
     res = elasticsearch.search(index="bx-book-ratings", from_=0, size=10000, query={
         "bool": {
-          "must": [
-            {
-              "match": {
-                "uid": f"{uid}"
-              }
-            }
+            "must": [
+                {
+                    "match": {
+                        "uid": f"{uid}"
+                    }
+                }
 
-          ]
+            ]
         }
-      })
+    })
 
     if (res["hits"]["total"]["value"] == 0):
         print("No data has been returned!")
@@ -103,13 +109,28 @@ def find_rated_books(uid):
     return res
 
 
+def form_training_data(user_id):
+    training_data_for_user = {}
+    rated_books = find_rated_books(user_id)
+    for book in rated_books["hits"]["hits"]:
+        book_search = basic_search("isbn", book["_source"]["isbn"])
+        if (book_search is None):
+            continue
+        summary = book_search["hits"]["hits"][0]["_source"]["summary"]
+        book_rating = user_rating_search(user_id, book["_source"]["isbn"])
+        tokenized_sentence = tokenize_sentence(summary)
+        training_data_for_user[str(tokenized_sentence)] = book_rating
+    return training_data_for_user
+
+
 # Testing
 # isbn: 0671789422
 # uid: 168047
+# 252071
 # Possessing the Secret of Joy
 if __name__ == "__main__":
 
-    training_data_for_user = {}
+    vocab_size = 10000
 
     print("Searching on index: bx-books.")
     print("You can search any field and value. Program ends with user_id input 'END'\n")
@@ -120,33 +141,38 @@ if __name__ == "__main__":
             print("Program exiting.")
             break
 
-        rated_books = find_rated_books(user_id)
-        for book in rated_books["hits"]["hits"]:
+        training_data = form_training_data(user_id=user_id)
+        print(len(training_data))
+        training_labels = np.array(list(training_data.values())) / 10
+        # print(training_data.keys())
+        encoded_data = [one_hot(sentence, vocab_size)
+                        for sentence in training_data.keys()]
+        # print(encoded_data)
 
-            book_search = basic_search("isbn", book["_source"]["isbn"])
-            if (book_search is None):
-                continue
-            summary = book_search["hits"]["hits"][0]["_source"]["summary"]
-            book_rating = user_rating_search(user_id, book["_source"]["isbn"])
-            tokenized_sentence = tokenize_sentence(summary)
+        # Variable max length for the padding
+        max_len = 0
+        for sentence in encoded_data:
+            if(len(sentence) > max_len):
+                max_len = len(sentence)
+        # print(max_len)
+        padded_data = pad_sequences(
+            encoded_data, maxlen=max_len, padding='post')
 
-            training_data_for_user[str(tokenized_sentence)] = book_rating
+        # Now we are ready to do embedding layer
 
-        print(len(training_data_for_user))
+        model = Sequential()
+        model.add(Embedding(vocab_size, 32, input_length=max_len))
+        model.add(Flatten())
+        model.add(Dense(100, activation='sigmoid'))
+        model.add(Dense(1, activation='sigmoid'))
+        model.compile(optimizer='adam', loss='mse',
+                      metrics=['accuracy'])
+        # print(model.summary())
 
-        for i in training_data_for_user:
-            print(i)
-            print(training_data_for_user[i])
-            print('\n')
+        # print(training_labels)
 
-
-
-
-
-
-
-
-
-
-
-
+        history = model.fit(padded_data, training_labels,
+                            epochs=20, verbose=1)
+        loss, accuracy = model.evaluate(
+            padded_data, training_labels, verbose=0)
+        print('Accuracy: %f' % (accuracy*100))
